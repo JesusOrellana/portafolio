1e350ff7443a42adc68a9808856c779514c06483
JesusOrellana <58049396+JesusOrellana@users.noreply.github.com>
2022-11-04 00:15:14 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Header } from './components/Header';
+import ReactLoading from 'react-loading';
+import './css/App.css';
+
+function App() {
+
+  const [ loader, setLoader ] = useState(true); 
+
+  if ( loader == true ) {
+    return (
+      <div className="Loading">
+  
+       <ReactLoading type='bubbles' color='#000000' height={'5%'} width={'10%'} />
+  
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Header></Header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../css/Header.css'
+
+export const Header = () => {
+  return (
+    <header> Aqui va el menu</header>
+  )
+}
